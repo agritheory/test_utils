4df5643c8a118cf89e83580545058a9508e40a40
Myuddin khatri <khatrimayu111@gmail.com>
2024-06-01 15:43:04 +0530

    feat: update clean_customized_doctypes to work as pre-commit hook

--- test_utils/pre_commit/clean_customized_doctypes.py
+++ test_utils/pre_commit/clean_customized_doctypes.py
@@ -1,18 +1,19 @@
+import argparse
 import datetime
 import json
 import os
 import pathlib
-import sys
 import tempfile
-
-from validate_customizations import scrub
+from typing import Sequence
+from test_utils.pre_commit.validate_customizations import scrub
 
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
+
 
 def get_customized_doctypes_to_clean(app):
 	customized_doctypes = {}
 
-	app_dir = pathlib.Path(__file__).resolve().parent.parent.parent / app
+	app_dir = pathlib.Path(app).resolve().parent
 	if not app_dir.is_dir():
 		return customized_doctypes
 
@@ -20,14 +21,17 @@ def get_customized_doctypes_to_clean(app):
 	for module in modules:
 		if not (app_dir / app / scrub(module) / "custom").exists():
 			continue
-	
-		for custom_file in list((app_dir / app / scrub(module) / "custom").glob("**/*.json")):
+
+		for custom_file in list(
+			(app_dir / app / scrub(module) / "custom").glob("**/*.json")
+		):
 			if custom_file.stem in customized_doctypes:
 				customized_doctypes[custom_file.stem].append(custom_file.resolve())
 			else:
 				customized_doctypes[custom_file.stem] = [custom_file.resolve()]
 
 	return customized_doctypes
+
 
 def validate_and_clean_customized_doctypes(customized_doctypes):
 	for doctype, customize_files in customized_doctypes.items():
@@ -39,10 +43,15 @@ def validate_and_clean_customized_doctypes(customized_doctypes):
 					if isinstance(value, list):
 						for item in value:
 							for item_key, item_value in list(item.items()):
-								if item_value is None and item_key not in ["default", "value"]:
+								if item_value is None and item_key not in [
+									"default",
+									"value",
+								]:
 									del item[item_key]
 								if item_key == "modified":
-									item["modified"] = datetime.datetime.now().strftime(DATETIME_FORMAT)
+									item["modified"] = datetime.datetime.now().strftime(
+										DATETIME_FORMAT
+									)
 
 					elif value is None and key not in ["default", "value"]:
 						del file_contents[key]
@@ -50,7 +59,14 @@ def validate_and_clean_customized_doctypes(customized_doctypes):
 				temp_file.write(json.dumps(file_contents, indent="\t", sort_keys=True))
 				os.replace(temp_file_path, customize_file)
 
-if __name__ == "__main__":
-	if sys.argv[1]:
-		customized_doctypes = get_customized_doctypes_to_clean(sys.argv[1])
+
+def main(argv: Sequence[str] = None):
+	parser = argparse.ArgumentParser()
+	parser.add_argument("filenames", nargs="*")
+	parser.add_argument("--app", action="append", help="An argument for the hook")
+	args = parser.parse_args(argv)
+
+	app = args.app[0]
+	if app:
+		customized_doctypes = get_customized_doctypes_to_clean(app)
 		validate_and_clean_customized_doctypes(customized_doctypes)
