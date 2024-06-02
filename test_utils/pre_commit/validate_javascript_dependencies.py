61032cb10ad8b8e713d6c44b367b1e4e757f3d43
Myuddin khatri <khatrimayu111@gmail.com>
2024-06-02 12:12:36 +0530

    feat: update validate_javascript_dependencies to work as pre-commit hook

--- test_utils/pre_commit/validate_javascript_dependencies.py
+++ test_utils/pre_commit/validate_javascript_dependencies.py
@@ -1,21 +1,28 @@
-
+import argparse
 import json
 import pathlib
 import sys
+from typing import Sequence
+
 
 def get_package_json(app):
-	apps_dir = pathlib.Path(__file__).resolve().parent.parent.parent
+	apps_dir = pathlib.Path().resolve().parent
 	if pathlib.Path.exists(apps_dir / app / "package.json"):
 		with open(apps_dir / app / "package.json") as f:
 			return json.load(f)
 
+
 def get_versions(app):
 	package_json = get_package_json(app)
 	if package_json:
-		return {**package_json.get("dependencies", {}), **package_json.get("devDependencies", {})}
+		return {
+			**package_json.get("dependencies", {}),
+			**package_json.get("devDependencies", {}),
+		}
+
 
 def get_mismatched_versions():
-	apps_order = pathlib.Path(__file__).resolve().parent.parent.parent.parent / "sites" / "apps.txt"
+	apps_order = pathlib.Path().resolve().parent.parent / "sites" / "apps.txt"
 	apps_order = apps_order.read_text().split("\n")
 	exceptions = []
 	app_packages = {app: get_versions(app) for app in apps_order}
@@ -31,9 +38,15 @@ def get_mismatched_versions():
 				if app == app2:
 					continue
 
-				if package in app2_packages and app2_packages[package] != package_version:
+				if (
+					package in app2_packages
+					and app2_packages[package] != package_version
+				):
 					# Check if exception already exists
-					existing_exception = next((exception for exception in exceptions if package in exception), None)
+					existing_exception = next(
+						(exception for exception in exceptions if package in exception),
+						None,
+					)
 					if existing_exception:
 						existing_exception[package][app] = package_version
 					else:
@@ -42,14 +55,16 @@ def get_mismatched_versions():
 	return exceptions
 
 
-if __name__ == "__main__":
-	exceptions = get_mismatched_versions()
+def main(argv: Sequence[str] = None):
+	parser = argparse.ArgumentParser()
+	parser.add_argument("filenames", nargs="*")
+	args = parser.parse_args(argv)
 
+	exceptions = get_mismatched_versions()
 	if exceptions:
 		for exception in exceptions:
 			for package, apps in exception.items():
 				print(f"\nVersion mismatch for {package} in:")
 				for app, version in apps.items():
 					print(f"{app}: {version}")
-
-		sys.exit(1) if all(exceptions) else sys.exit(0)
+		sys.exit(1)
