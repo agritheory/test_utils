335501f61b01c29445b2c4e9ede2234e54e77b04
Myuddin Khatri <53251406+MyuddinKhatri@users.noreply.github.com>
2024-06-04 20:07:35 +0530

    feat(fixtures): fixtures data for customers (#39)
    * feat(fixtures): fixtures data for customers
    
    * fix: update create_customers and fixtures
    
    * chore: spelling for phoenix
    
    ---------
    
    Co-authored-by: Rohan Bansal <rohan@agritheory.dev>

--- test_utils/utils/setup_fixtures.py
+++ test_utils/utils/setup_fixtures.py
@@ -14,6 +14,64 @@ def get_fixtures_data_from_file(filename):
 			return json.load(f)
 
 
+def create_customers(settings, only_create=None):
+	customers = get_fixtures_data_from_file("customers.json")
+	addresses = get_fixtures_data_from_file("addresses.json")
+	users = get_fixtures_data_from_file("users.json")
+	contacts = get_fixtures_data_from_file("contacts.json")
+
+	for customer in customers:
+		if only_create and customer.get("customer_name") not in only_create:
+			continue
+
+		if frappe.db.exists("Customer", customer.get("customer_name")):
+			continue
+
+		cust = frappe.new_doc("Customer")
+		cust.update(customer)
+		cust.save()
+
+		for address in addresses:
+			existing_address = frappe.get_value(
+				"Address", {"address_line1": address.get("address_line1")}
+			)
+			if existing_address:
+				continue
+
+			for address_link in address.get("links"):
+				if (
+					address_link.get("link_doctype") == "Customer"
+					and address_link.get("link_name") == cust.customer_name
+				):
+					addr = frappe.new_doc("Address")
+					addr.update(address)
+					addr.save()
+
+		for user in users:
+			if frappe.db.exists("User", user.get("username")):
+				continue
+			for role in user.get("roles"):
+				if role.get("role") == "Customer":
+					user_doc = frappe.new_doc("User")
+					user_doc.update(user)
+					user_doc.save()
+
+		for contact in contacts:
+			existing_contact = frappe.db.get_value(
+				"Contact", {"email_id": customer.get("email")}
+			)
+			if existing_contact:
+				continue
+			for contact_link in contact.get("links"):
+				if (
+					contact_link.get("link_doctype") == "Customer"
+					and contact_link.get("link_name") == cust.customer_name
+				):
+					contact_doc = frappe.new_doc("Contact")
+					contact_doc.update(contact)
+					contact_doc.save()
+
+
 def create_suppliers(settings, only_create=None):
 	suppliers = get_fixtures_data_from_file("suppliers.json")
 	addresses = get_fixtures_data_from_file("addresses.json")
