0d6c8a3e0fc31ff25ef332b729f737afc0aaf9de
Myuddin khatri <khatrimayu111@gmail.com>
2024-07-02 10:50:03 +0530

    feat: add translations

---
+++ test_utils/pre_commit/add_translations.py
@@ -0,0 +1,26 @@
+import argparse
+from typing import Sequence
+
+try:
+	from frappe.translate import get_untranslated, update_translations
+except Exception as e:
+	raise(e)
+
+
+def add_translations(lang, app):
+	untranslated_file = "untranslated_strings"
+	translated_file = "translated_strings"
+	get_untranslated(lang=lang, untranslated_file=untranslated_file, app=app)
+	update_translations(lang=lang, untranslated_file=untranslated_file, translated_file=translated_file, app=app)
+
+
+def main(argv: Sequence[str] = None):
+	parser = argparse.ArgumentParser()
+	parser.add_argument('filenames', nargs='*')
+	parser.add_argument('--lang', action='append', help='Language to translate strings')
+	parser.add_argument('--app', action='append', help='App to get untranslated string and translate them')
+	args = parser.parse_args(argv)
+
+	lang = args.lang[0]
+	app = args.app[0]
+	add_translations(lang, app)
