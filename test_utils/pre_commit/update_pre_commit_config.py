8ee41dda35e150e53666bd0f85970a382919ee2f
Myuddin Khatri <53251406+MyuddinKhatri@users.noreply.github.com>
2024-07-13 18:12:52 +0530

    feat: auto update pre-commit config (#59)
    * feat: auto update pre-commit config
    
    * docs: update_pre_commit_config

---
+++ test_utils/pre_commit/update_pre_commit_config.py
@@ -0,0 +1,19 @@
+
+import subprocess
+import sys
+
+def main():
+	try:
+		# Run the pre-commit autoupdate with --repo (path to test_utils) to update pre-commit config
+		result = subprocess.run(["pre-commit", "autoupdate", "--repo", "https://github.com/agritheory/test_utils"], capture_output=True, text=True)
+
+		print(result.stdout)
+		if result.stderr:
+			print(result.stderr)
+
+	except Exception as e:
+		print(f"Error: {e}")
+		sys.exit(1)
+
+if __name__ == "__main__":
+	main()
